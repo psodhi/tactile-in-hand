d8545cf4afa5cc8a95f061ce5d5d3d0e748fe0ec
vocdex <nazirjonovsh2000@gmail.com>
2022-06-20 15:27:31 +0900

    editing

--- testfile.py
+++ testfile.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f"Hello, {name}")
-    print(f"Hello, bro {name}")
+    print(f"Hello, {name}. How are you doing?")
 
 
 # Press the green button in the gutter to run the script.
